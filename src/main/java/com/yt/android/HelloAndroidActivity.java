e9b6a1380e35b2d618085f3e35681cbed423edef
11653786@qq.com <yangtao>
2016-04-08 17:42:22 +0800

    6666

--- src/main/java/com/yt/android/HelloAndroidActivity.java
+++ src/main/java/com/yt/android/HelloAndroidActivity.java
@@ -1,29 +1,16 @@
 package com.yt.android;
 
-import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v4.app.FragmentActivity;
 import android.support.v4.view.ViewPager;
-import android.widget.ImageView;
 import android.widget.RadioButton;
 import android.widget.RadioGroup;
 import com.yt.android.activity.IndexActivity;
-import com.yt.android.adapter.ViewPagerAdapter;
-import com.yt.android.contains.Contains;
-
-import java.util.ArrayList;
 
 public class HelloAndroidActivity extends FragmentActivity implements RadioGroup.OnCheckedChangeListener {
 
     private ViewPager viewpager;
-    private ArrayList<ImageView> imageViews;
-    private int previousSelectPosition = 0;
-    private boolean toRight = true;
-    //是否向左开始自动播放
-    private boolean toLeft = false;
-    private int lunbopos = 0;
-    //底部菜单
     //我的,地图,首页
     private RadioGroup radioGroup;
     private RadioButton tabHome, tabMap, tabMy;
