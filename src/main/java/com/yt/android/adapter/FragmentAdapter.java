d4b1a5669ca83327dd8df72ef36e5aa00c7e2dcd
11653786@qq.com <yangtao>
2016-04-08 17:21:03 +0800

    6666

---
+++ src/main/java/com/yt/android/adapter/FragmentAdapter.java
@@ -0,0 +1,39 @@
+package com.yt.android.adapter;
+
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentPagerAdapter;
+
+import java.util.List;
+
+/**
+ * @author zhangsan
+ * @version 1.0
+ * @package com.yt.android.adapter
+ * @date 2016/4/8 0008 17:14
+ * @descption: 疯狂的王麻子团队!
+ */
+public class FragmentAdapter extends FragmentPagerAdapter {
+
+    private List<Fragment> fragments;
+
+
+    public FragmentAdapter(FragmentManager fm) {
+        super(fm);
+    }
+
+    public FragmentAdapter(FragmentManager fm, List<Fragment> fragments) {
+        super(fm);
+        this.fragments = fragments;
+    }
+
+    @Override
+    public Fragment getItem(int position) {
+        return fragments.get(position);
+    }
+
+    @Override
+    public int getCount() {
+        return fragments.size();
+    }
+}
