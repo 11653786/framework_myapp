5ae3c814677dbc9bdd674bc8d88a40693edcc9e6
11653786@qq.com <yangtao>
2016-04-08 17:26:23 +0800

    6666

--- src/main/java/com/yt/android/HelloAndroidActivity.java
+++ src/main/java/com/yt/android/HelloAndroidActivity.java
@@ -2,23 +2,17 @@ package com.yt.android;
 
 import android.app.Activity;
 import android.os.Bundle;
-import android.os.Handler;
-import android.os.Message;
+import android.support.v4.app.FragmentActivity;
 import android.support.v4.view.ViewPager;
-import android.view.MotionEvent;
-import android.view.View;
 import android.widget.ImageView;
 import android.widget.RadioButton;
 import android.widget.RadioGroup;
-import com.yt.android.adapter.TabHostAdapter;
 import com.yt.android.adapter.ViewPagerAdapter;
 import com.yt.android.contains.Contains;
 
 import java.util.ArrayList;
-import java.util.Timer;
-import java.util.TimerTask;
 
-public class HelloAndroidActivity extends Activity implements ViewPager.OnPageChangeListener, View.OnTouchListener {
+public class HelloAndroidActivity extends FragmentActivity {
 
     private ViewPager viewpager;
     private ArrayList<ImageView> imageViews;
@@ -27,9 +21,7 @@ public class HelloAndroidActivity extends Activity implements ViewPager.OnPageCh
     //是否向左开始自动播放
     private boolean toLeft = false;
     private int lunbopos = 0;
-    ImageTimer task = null;
     //底部菜单
-    private TabHostAdapter adapter;
     //我的,地图,首页
     private RadioGroup radioGroup;
     private RadioButton indexButton;
@@ -43,11 +35,6 @@ public class HelloAndroidActivity extends Activity implements ViewPager.OnPageCh
         setContentView(R.layout.activity_main);
         initView();
         viewpager.setAdapter(new ViewPagerAdapter(imageViews));
-        viewpager.setOnPageChangeListener(this);
-        viewpager.setOnTouchListener(this);
-        // 自动切换页面功能
-        task = new ImageTimer();
-        timer.schedule(task, 1000, 2000); // 1s后执行task,经过1s再次执行
     }
 
     private void initView() {
@@ -58,111 +45,6 @@ public class HelloAndroidActivity extends Activity implements ViewPager.OnPageCh
         mapButton = (RadioButton) findViewById(R.id.mapactivity);
         myButton = (RadioButton) findViewById(R.id.myactivity);
     }
-
-
-    @Override
-    public void onPageScrollStateChanged(int position) {
-    }
-
-    @Override
-    public void onPageScrolled(int arg0, float arg1, int arg2) {
-    }
-
-    @Override
-    public void onPageSelected(int position) {
-        previousSelectPosition = position;
-    }
-
-
-    /**
-     * 点击屏幕轮播图暂停
-     *
-     * @param view
-     * @param motionEvent
-     * @return
-     */
-    @Override
-    public boolean onTouch(View view, MotionEvent motionEvent) {
-        switch (motionEvent.getAction()) {
-            case MotionEvent.ACTION_DOWN:
-                task.setTrue(false);
-                break;
-            case MotionEvent.ACTION_UP:
-                task.setTrue(true);
-                break;
-            case MotionEvent.ACTION_MOVE:
-                task.setTrue(false);
-                break;
-            default:
-                break;
-        }
-        return super.onTouchEvent(motionEvent);
-    }
-
-
-    //轮播图相关的定时器
-    Handler handler = new Handler() {
-        public void handleMessage(Message msg) {
-            if (msg.what == 1) {
-                //设置轮播图必须放在这里
-                if (viewpager != null) {
-                    viewpager.setCurrentItem(lunbopos);
-                }
-            }
-        }
-
-        ;
-    };
-    Timer timer = new Timer();
-
-    class ImageTimer extends TimerTask {
-
-        private boolean isTrue = true;
-
-        @Override
-        public void run() {
-            //轮播
-            lunbo(isTrue);
-            //发送消息
-            Message message = new Message();
-            message.what = 1;
-            handler.sendMessage(message);
-        }
-
-        public void setTrue(boolean isTrue) {
-            this.isTrue = isTrue;
-        }
-    }
-
-    ;
-
-    /**
-     * 轮播运转方法
-     */
-    public void lunbo(boolean isTrue) {
-        if (isTrue) {
-            if (lunbopos <= 4) {
-                if (lunbopos == 4) {
-                    toRight = false;
-                    toLeft = true;
-                }
-                if (lunbopos == 0) {
-                    toRight = true;
-                    toLeft = false;
-                }
-
-                if (toRight) {
-                    lunbopos++;
-                }
-                if (toLeft) {
-                    lunbopos--;
-                }
-            }
-        }
-
-    }
-
-
 }
 
 
