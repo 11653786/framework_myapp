859d5b5f6d53c4b209213d15dd5cb15b3faeb515
11653786@qq.com <yangtao>
2016-04-08 09:14:47 +0800

    first commit

---
+++ src/main/java/com/yt/android/base/BaseActivity.java
@@ -0,0 +1,13 @@
+package com.yt.android.base;
+
+import android.app.Activity;
+
+/**
+ * @author zhangsan
+ * @version 1.0
+ * @package com.yt.android.base
+ * @date 2016/4/6 0006 10:59
+ * @descption: 疯狂的王麻子团队!
+ */
+public class BaseActivity extends Activity {
+}
