dc7a3a3217f84f7fd525cc46fe68296a05f959b2
11653786@qq.com <yangtao>
2016-04-11 11:29:56 +0800

    6666

--- src/main/java/com/yt/android/HelloAndroidActivity.java
+++ src/main/java/com/yt/android/HelloAndroidActivity.java
@@ -1,22 +1,43 @@
 package com.yt.android;
 
 import android.os.Bundle;
+import android.os.Handler;
+import android.os.Message;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentActivity;
 import android.support.v4.view.ViewPager;
+import android.view.MotionEvent;
+import android.view.View;
+import android.widget.ImageView;
 import android.widget.RadioButton;
 import android.widget.RadioGroup;
 import com.yt.android.adapter.FragmentAdapter;
+import com.yt.android.adapter.ViewPagerAdapter;
+import com.yt.android.contains.Contains;
 import com.yt.android.fragment.HomeFragment;
 import com.yt.android.fragment.MyFragment2;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Timer;
+import java.util.TimerTask;
 
 /**
  * 第一个activity
  */
-public class HelloAndroidActivity extends FragmentActivity implements RadioGroup.OnCheckedChangeListener {
+public class HelloAndroidActivity extends FragmentActivity implements RadioGroup.OnCheckedChangeListener, View.OnTouchListener {
+
+
+    //轮播图相关
+    private ViewPager lunbo;
+    private ArrayList<ImageView> imageViews;
+    private int previousSelectPosition = 0;
+    private boolean toRight = true;
+    //是否向左开始自动播放
+    private boolean toLeft = false;
+    private int lunbopos = 0;
+    //定时器控制自动轮播
+    ImageTimer task = null;
 
     private ViewPager viewpager;
     //我的,地图,首页
@@ -33,19 +54,40 @@ public class HelloAndroidActivity extends FragmentActivity implements RadioGroup
     }
 
     private void initView() {
+        initLunbo();
         viewpager = (ViewPager) findViewById(R.id.tabHost);
+        //底部导航菜单
         radioGroup = (RadioGroup) findViewById(R.id.radiogroup);
         tabHome = (RadioButton) findViewById(R.id.tabHome);
         tabMap = (RadioButton) findViewById(R.id.tabMap);
         tabMy = (RadioButton) findViewById(R.id.tabMy);
         List<Fragment> fragments = getFragments();
+        //fragmentAdapter初始化
         FragmentAdapter adapter = new FragmentAdapter(getSupportFragmentManager(), fragments);
         viewpager.setAdapter(adapter);
         radioGroup.setOnCheckedChangeListener(this);
         viewpager.setCurrentItem(0);
     }
 
+    /**
+     * 初始化轮播图方法
+     */
+    private void initLunbo() {
+        lunbo = (ViewPager) findViewById(R.id.lunbotu);
+        imageViews = Contains.setImageViews(getApplicationContext());
+        lunbo.setAdapter(new ViewPagerAdapter(imageViews));
+        lunbo.setOnTouchListener(this);
+        // 自动切换页面功能
+        task = new ImageTimer();
+        timer.schedule(task, 1000, 2000); // 1s后执行task,经过1s再次执行
+    }
 
+
+    /**
+     * 获取要展示的Fragment
+     *
+     * @return
+     */
     private List<Fragment> getFragments() {
         HomeFragment homeFragment = new HomeFragment();
         MyFragment2 fragment2 = new MyFragment2();
@@ -55,6 +97,13 @@ public class HelloAndroidActivity extends FragmentActivity implements RadioGroup
         return fragments;
     }
 
+
+    /**
+     * 底部导航菜单变更方法
+     *
+     * @param radioGroup
+     * @param i
+     */
     @Override
     public void onCheckedChanged(RadioGroup radioGroup, int i) {
         switch (i) {
@@ -78,6 +127,98 @@ public class HelloAndroidActivity extends FragmentActivity implements RadioGroup
                 break;
         }
     }
+
+
+    /**
+     * 点击屏幕轮播图暂停
+     *
+     * @param view
+     * @param motionEvent
+     * @return
+     */
+    @Override
+    public boolean onTouch(View view, MotionEvent motionEvent) {
+        switch (motionEvent.getAction()) {
+            case MotionEvent.ACTION_DOWN:
+                task.setTrue(false);
+                break;
+            case MotionEvent.ACTION_UP:
+                task.setTrue(true);
+                break;
+            case MotionEvent.ACTION_MOVE:
+                task.setTrue(false);
+                break;
+            default:
+                break;
+        }
+        return true;
+    }
+
+
+    //轮播图相关的定时器
+    Handler handler = new Handler() {
+        public void handleMessage(Message msg) {
+            if (msg.what == 1) {
+                //设置轮播图必须放在这里
+                if (lunbo != null) {
+                    lunbo.setCurrentItem(lunbopos);
+                }
+            }
+        }
+
+        ;
+    };
+    Timer timer = new Timer();
+
+
+    class ImageTimer extends TimerTask {
+
+        private boolean isTrue = true;
+
+        @Override
+        public void run() {
+            //轮播
+            lunbo(isTrue);
+            //发送消息
+            Message message = new Message();
+            message.what = 1;
+            handler.sendMessage(message);
+        }
+
+        public void setTrue(boolean isTrue) {
+            this.isTrue = isTrue;
+        }
+    }
+
+    ;
+
+    /**
+     * 轮播运转方法,根据状态来左右轮播
+     */
+    public void lunbo(boolean isTrue) {
+        if (isTrue) {
+            if (lunbopos <= 4) {
+                if (lunbopos == 4) {
+                    toRight = false;
+                    toLeft = true;
+                }
+                if (lunbopos == 0) {
+                    toRight = true;
+                    toLeft = false;
+                }
+
+                if (toRight) {
+                    lunbopos++;
+                }
+                if (toLeft) {
+                    lunbopos--;
+                }
+            }
+        }
+
+    }
+
+
 }
 
 
--- src/main/java/com/yt/android/fragment/HomeFragment.java
+++ src/main/java/com/yt/android/fragment/HomeFragment.java
@@ -27,17 +27,7 @@ import java.util.TimerTask;
  * @date 2016/4/8 0008 17:43
  * @descption: 疯狂的王麻子团队!
  */
-public class HomeFragment extends Fragment implements View.OnTouchListener {
-
-
-    private ViewPager lunbo;
-    private ArrayList<ImageView> imageViews;
-    private int previousSelectPosition = 0;
-    private boolean toRight = true;
-    //是否向左开始自动播放
-    private boolean toLeft = false;
-    private int lunbopos = 0;
-    ImageTimer task = null;
+public class HomeFragment extends Fragment {
 
 
     @Override
@@ -49,107 +39,9 @@ public class HomeFragment extends Fragment implements View.OnTouchListener {
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         // 第一个参数是这个Fragment将要显示的界面布局,第二个参数是这个Fragment所属的Activity,第三个参数是决定此fragment是否附属于Activity
         View view = inflater.inflate(R.layout.activity_main, container, false);
-        initLunbo(view);
+        // initLunbo(view);
         return view;
     }
 
-    private void initLunbo(View view) {
-        lunbo = (ViewPager) view.findViewById(R.id.lunbotu);
-        imageViews = Contains.setImageViews(getActivity().getApplicationContext());
-        lunbo.setAdapter(new ViewPagerAdapter(imageViews));
-        lunbo.setOnTouchListener(this);
-        // 自动切换页面功能
-        task = new ImageTimer();
-        timer.schedule(task, 1000, 2000); // 1s后执行task,经过1s再次执行
-    }
-
-
-    /**
-     * 点击屏幕轮播图暂停
-     *
-     * @param view
-     * @param motionEvent
-     * @return
-     */
-    @Override
-    public boolean onTouch(View view, MotionEvent motionEvent) {
-        switch (motionEvent.getAction()) {
-            case MotionEvent.ACTION_DOWN:
-                task.setTrue(false);
-                break;
-            case MotionEvent.ACTION_UP:
-                task.setTrue(true);
-                break;
-            case MotionEvent.ACTION_MOVE:
-                task.setTrue(false);
-                break;
-            default:
-                break;
-        }
-        return true;
-    }
-
-
-    //轮播图相关的定时器
-    Handler handler = new Handler() {
-        public void handleMessage(Message msg) {
-            if (msg.what == 1) {
-                //设置轮播图必须放在这里
-                if (lunbo != null) {
-                    lunbo.setCurrentItem(lunbopos);
-                }
-            }
-        }
-
-        ;
-    };
-    Timer timer = new Timer();
-
-    class ImageTimer extends TimerTask {
-
-        private boolean isTrue = true;
-
-        @Override
-        public void run() {
-            //轮播
-            lunbo(isTrue);
-            //发送消息
-            Message message = new Message();
-            message.what = 1;
-            handler.sendMessage(message);
-        }
-
-        public void setTrue(boolean isTrue) {
-            this.isTrue = isTrue;
-        }
-    }
-
-    ;
-
-    /**
-     * 轮播运转方法
-     */
-    public void lunbo(boolean isTrue) {
-        if (isTrue) {
-            if (lunbopos <= 4) {
-                if (lunbopos == 4) {
-                    toRight = false;
-                    toLeft = true;
-                }
-                if (lunbopos == 0) {
-                    toRight = true;
-                    toLeft = false;
-                }
-
-                if (toRight) {
-                    lunbopos++;
-                }
-                if (toLeft) {
-                    lunbopos--;
-                }
-            }
-        }
-
-    }
 
 }
