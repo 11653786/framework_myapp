73fd6dd9ec90c14d8f15e7add06e8aadd14f8599
11653786@qq.com <yangtao>
2016-04-11 14:03:41 +0800

    图片居中

---
+++ src/main/java/com/yt/android/activity/map/MapActivity.java
@@ -0,0 +1,19 @@
+package com.yt.android.activity.map;
+
+import android.os.Bundle;
+import com.yt.android.R;
+import com.yt.android.base.BaseActivity;
+
+public class MapActivity extends BaseActivity {
+
+	@Override
+	public void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+        setContentView(R.layout.fragment_home);
+
+	}
+
+
+
+}
+
