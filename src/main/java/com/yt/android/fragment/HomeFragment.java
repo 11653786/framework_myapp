76dd2a4e8ec3c9104dc4d1825f6750e1160d4b73
11653786@qq.com <yangtao>
2016-04-12 09:40:49 +0800

    666

--- src/main/java/com/yt/android/fragment/HomeFragment.java
+++ src/main/java/com/yt/android/fragment/HomeFragment.java
@@ -5,6 +5,7 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
 import com.yt.android.R;
 
 /**
@@ -16,7 +17,9 @@ import com.yt.android.R;
  * @date 2016/4/8 0008 17:43
  * @descption: 疯狂的王麻子团队!
  */
-public class HomeFragment extends Fragment {
+public class HomeFragment extends Fragment implements View.OnClickListener {
+
+    private ImageView news,xuexiaogaikuang,yuanxijieshao,xiaoyuanfengcai;
 
 
     @Override
@@ -28,9 +31,17 @@ public class HomeFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         // 第一个参数是这个Fragment将要显示的界面布局,第二个参数是这个Fragment所属的Activity,第三个参数是决定此fragment是否附属于Activity
         View view = inflater.inflate(R.layout.fragment_home, container, false);
-        // initLunbo(view);
+        initView(view);
         return view;
     }
 
+    public void initView(View view) {
 
+    }
+
+
+    @Override
+    public void onClick(View view) {
+
+    }
 }
