5b4c24abad79038265f662b872c0da4b2176faec
11653786@qq.com <yangtao>
2016-04-13 14:54:27 +0800

    666

--- src/main/java/com/yt/android/application/MyApplication.java
+++ src/main/java/com/yt/android/application/MyApplication.java
@@ -3,14 +3,18 @@ package com.yt.android.application;
 
 import android.app.Application;
 import com.baidu.mapapi.SDKInitializer;
+import com.iflytek.cloud.SpeechUtility;
+import com.yt.android.R;
 
 /**
- * 百度地图初始化服务
+ * 百度地图，科大讯飞初始化服务
  */
 public class MyApplication extends Application {
 
     @Override
     public void onCreate() {
+        //科大讯飞
+        SpeechUtility.createUtility(MyApplication.this, "appid=" + getString(R.string.app_id));
         super.onCreate();
         // 在使用 SDK 各组间之前初始化 context 信息，传入 ApplicationContext
         SDKInitializer.initialize(this);
